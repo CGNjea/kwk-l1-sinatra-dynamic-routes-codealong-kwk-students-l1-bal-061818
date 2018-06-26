994dfd0eadb95d5a3a326e248e13b8e25555fab8
CGNjea <aryeejodi@gmail.com>
2018-06-26 19:55:18 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -14,5 +14,18 @@ class App < Sinatra::Base
   end
 
   # Code your final two routes here:
-
+get '/:operation/:number1/:number2' do
+  @operation = params[:operation]
+  @number1 = params[:number1]
+  @number2 = params[:number2]
+  
+  if @operation == "add"
+    @number1 + @number2
+  elsif @operation == "subtract"
+    @number1 - @number2
+  elsif @operation == "multiply"
+    @number1 * @number2
+  else 
+    @number1 / @number2
+  end 
 end
